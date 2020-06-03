70ea7b50b013f0d07083997dda1c5f8e4e75e809
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2020-06-03 14:12:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,141 @@
+document.querySelector('#profile_photo').setAttribute('src', 'assets/profile' + Math.floor(Math.random() * (3 - 1) + 1) + '.png')
+
+const contactContainer = document.querySelector('#contact_container')
+const contactBt1 = document.querySelector('#contact_bts span');
+const contactBt2 = document.querySelector('#contact_bts a:nth-child(2)');
+const contactBt3 = document.querySelector('#contact_bts a:nth-child(3)');
+const contactBt4 = document.querySelector('#contact_bts a:nth-child(4)');
+const contactBts = document.querySelectorAll('#contact_bts > *');
+
+const preTimeline = document.querySelector('#pre_timeline');
+const posTimeline = document.querySelector('#pos_timeline');
+
+const iconBGColor = document.querySelectorAll('.iconBg');
+const iconColor = document.querySelectorAll('.iconBody');
+const measurers = document.querySelectorAll('#measurers img');
+
+let contactsOpen = false;
+
+//Checking states with scroll position
+function checkScroll() {
+
+    let preTimelinePosition = preTimeline.getBoundingClientRect().y;
+    let posTimelinePosition = posTimeline.getBoundingClientRect().y;
+    
+    let windowPosition = window.pageYOffset;
+
+    if(window.matchMedia("(max-width: 600px)").matches) {
+    
+        windowWidthProfilePhoto = window.innerWidth / 100 * 35;
+    
+        const windowHeight = window.innerHeight / 1.1;
+    
+        if(preTimelinePosition < window.innerHeight * 0.9 && posTimelinePosition > window.innerHeight * 0.9) {
+            for(i = 0; i < measurers.length; ++i) {
+                measurers[i].style.filter = 'brightness(5)';
+            }
+    
+            for(i = 0; i < iconBGColor.length; ++i) {
+                iconBGColor[i].style.fill = '#4E4CFF';
+            }
+            for(i = 0; i < iconColor.length; ++i) {
+                iconColor[i].style.fill = '#ffffff';
+            }
+            } else {
+                for(i = 0; i < measurers.length; ++i) {
+                    measurers[i].style.filter = 'brightness(1)';
+                }
+                
+                for(i = 0; i < contactBts.length; ++i) {
+                    contactBts[i].style.transform = 'translate(0,0)';
+                }
+                
+                contactContainer.style.transform = 'scale(0)'
+    
+                for(i = 0; i < iconBGColor.length; ++i) {
+                    iconBGColor[i].style.fill = '#ffffff';
+                }
+    
+                for(i = 0; i < iconColor.length; ++i) {
+                    iconColor[i].style.fill = '#4E4CFF';
+                }
+            }
+
+            
+
+            contactContainer.addEventListener('click', function() {
+                if(contactsOpen == false) {
+                    contactContainer.style.transform = 'scale(1) rotate(225deg)'
+                    contactsOpen = true;
+
+                    contactBt1.style.transform = 'translate(2vw, calc(100vh - 12rem))'
+                    contactBt2.style.transform = 'translate(-10vw, calc(100vh - 15rem))'
+                    contactBt3.style.transform = 'translate(-63vw, calc(100vh - 18rem))'
+                    contactBt4.style.transform = 'translate(-75vw, calc(100vh - 21rem))'
+                } else {
+                    contactContainer.style.transform = 'scale(1)'
+                    contactsOpen = false;
+
+                    contactBt1.style.transform = 'translate(2vw, calc(100vh - 8rem))'
+                    contactBt2.style.transform = 'translate(-10vw, calc(100vh - 8rem))'
+                    contactBt3.style.transform = 'translate(-64vw, calc(100vh - 8rem))'
+                    contactBt4.style.transform = 'translate(-75vw, calc(100vh - 8rem))'
+                }
+            })
+
+            if(windowPosition > windowWidthProfilePhoto) {
+    
+                contactsOpen = false;
+                contactContainer.style.transform = 'scale(1)'
+    
+                contactBt1.style.transform = 'translate(2vw, calc(100vh - 8rem))'
+                contactBt2.style.transform = 'translate(-10vw, calc(100vh - 8rem))'
+                contactBt3.style.transform = 'translate(-64vw, calc(100vh - 8rem))'
+                contactBt4.style.transform = 'translate(-75vw, calc(100vh - 8rem))'
+    
+                contactContainer.style.transform = 'scale(1)'
+            }
+
+    } else {
+        if(preTimelinePosition < window.innerHeight * 0.05 && posTimelinePosition > window.innerHeight * 0.05) {
+            for(i = 0; i < measurers.length; ++i) {
+                measurers[i].style.filter = 'brightness(5)';
+            }
+    
+            for(i = 0; i < iconBGColor.length; ++i) {
+                iconBGColor[i].style.fill = '#4E4CFF';
+            }
+            for(i = 0; i < iconColor.length; ++i) {
+                iconColor[i].style.fill = '#ffffff';
+            }
+            } else {
+                for(i = 0; i < measurers.length; ++i) {
+                    measurers[i].style.filter = 'brightness(1)';
+                }
+                
+                for(i = 0; i < contactBts.length; ++i) {
+                    contactBts[i].style.transform = 'translate(0,0)';
+                }
+    
+                // contactBt1.style.transform = 'translate(7vw, 0)'
+                // contactBt2.style.transform = 'translate(19vw, 0)'
+                // contactBt3.style.transform = 'translate(72vw, 0)'
+                // contactBt4.style.transform = 'translate(84vw, 0)'
+                
+                contactContainer.style.transform = 'scale(0)'
+    
+                for(i = 0; i < iconBGColor.length; ++i) {
+                    iconBGColor[i].style.fill = '#ffffff';
+                }
+    
+                for(i = 0; i < iconColor.length; ++i) {
+                    iconColor[i].style.fill = '#4E4CFF';
+                }
+            }
+    }
+}
+
+checkScroll()
 document.onreadystatechange = function () {
     if (document.readyState == "interactive" || document.readyState == "complete") {
 
@@ -9,100 +147,15 @@ document.onreadystatechange = function () {
             loadImg.style.display = 'none';
         }, 1000);
 
-        const preTimeline = document.querySelector('#pre_timeline');
-        const posTimeline = document.querySelector('#pos_timeline');
-        const measurers = document.querySelectorAll('#measurers img');
-
-        let preTimelinePosition = preTimeline.offsetTop;
-        let posTimelinePosition = posTimeline.offsetTop;
-
-        windowWidthProfilePhoto = window.innerWidth / 100 * 35;
-
-        const contactBts = document.querySelectorAll('#contact_bts > *');
-
-        const iconBGColor = document.querySelectorAll('.iconBg');
-        const iconColor = document.querySelectorAll('.iconBody');
-
-        const contactContainer = document.querySelector('#contact_container')
-        const contactBt1 = document.querySelector('#contact_bts span');
-        const contactBt2 = document.querySelector('#contact_bts a:nth-child(2)');
-        const contactBt3 = document.querySelector('#contact_bts a:nth-child(3)');
-        const contactBt4 = document.querySelector('#contact_bts a:nth-child(4)');
-
-        const windowHeight = window.innerHeight / 1.1;
-
 
         document.body.onscroll = function() {
-            let windowPosition = window.pageYOffset;
+            checkScroll()
 
-            if(windowPosition > preTimelinePosition - windowHeight && windowPosition < posTimelinePosition - windowHeight) {
-                for(i = 0; i < measurers.length; ++i) {
-                    measurers[i].style.filter = 'brightness(5)';
-                }
-
-                for(i = 0; i < iconBGColor.length; ++i) {
-                    iconBGColor[i].style.fill = '#4E4CFF';
-                }
-                for(i = 0; i < iconColor.length; ++i) {
-                    iconColor[i].style.fill = '#ffffff';
-                }
-                } else {
-                    for(i = 0; i < measurers.length; ++i) {
-                        measurers[i].style.filter = 'brightness(1)';
-                    }
-
-                    contactBt1.style.transform = 'translate(7vw, 0)'
-                    contactBt2.style.transform = 'translate(19vw, 0)'
-                    contactBt3.style.transform = 'translate(72vw, 0)'
-                    contactBt4.style.transform = 'translate(84vw, 0)'
-                    
-                    contactContainer.style.transform = 'scale(0)'
-
-                    for(i = 0; i < iconBGColor.length; ++i) {
-                        iconBGColor[i].style.fill = '#ffffff';
-                    }
-
-                    for(i = 0; i < iconColor.length; ++i) {
-                        iconColor[i].style.fill = '#4E4CFF';
-                    }
-                }
-
-                if(windowPosition > windowWidthProfilePhoto) {
-
-                    contactsOpen = false;
-                    contactContainer.style.transform = 'scale(1)'
-                    
-                    for(i = 0; i < contactBts.length; ++i) {
-                        contactBts[i].style.transform = 'translate(9vw, calc(100vh - 6.3rem))';
-                    }
-
-                    contactContainer.style.transform = 'scale(1)'
-                }
+            document.querySelector('#timeline').style.transform = 'translateY( -' + window.pageYOffset / 3 + 'px)';
         }
-
-        let contactsOpen = false;
-        
-        contactContainer.addEventListener('click', function() {
-            if(contactsOpen == false) {
-                contactContainer.style.transform = 'scale(1) rotate(225deg)'
-                contactsOpen = true;
-                contactBt1.style.transform = 'translate(25vw, calc(100vh - 6rem))'
-                contactBt2.style.transform = 'translate(37vw, calc(100vh - 6rem))'
-                contactBt3.style.transform = 'translate(49vw, calc(100vh - 6rem))'
-                contactBt4.style.transform = 'translate(61vw, calc(100vh - 6rem))'
-            } else {
-                contactContainer.style.transform = 'scale(1)'
-                contactsOpen = false;
-                for(i = 0; i < contactBts.length; ++i) {
-                    contactBts[i].style.transform = 'translate(9vw, calc(100vh - 6rem))';
-                }
-            }
-        })
 
         const emailBt = document.querySelector('#email_bt');
         const form = document.querySelector('#form_container');
-
-        form.style.transition = 'transform 0.6s cubic-bezier(0.4, 0, 0.2, 1)';
 
         const closeForm = document.querySelector('#close_form');
         const sendEmail = document.querySelector('#send_bt');
@@ -118,28 +171,35 @@ document.onreadystatechange = function () {
                     closeForm.style.opacity = '1';
                     sendEmail.style.transform = 'translateX(0)';
                     sendEmail.style.opacity = '1';
-                }, 400);
+                }, 200);
             } else {
-                emailOpen = false;
-                form.style.transform = 'translateY(-150%)';
-                setTimeout(() => {
-                    closeForm.style.transform = 'translateX(-6rem)';
-                    closeForm.style.opacity = '0';
-                    sendEmail.style.transform = 'translateX(6rem)';
-                    sendEmail.style.opacity = '0';
-                }, 400);
-
+                dismissEmail()
             }
         })
+        
+
+        function dismissEmail() {
+            emailOpen = false;
+                closeForm.style.transform = 'translateX(-6rem)';
+                closeForm.style.opacity = '0';
+                sendEmail.style.transform = 'translateX(6rem)';
+                sendEmail.style.opacity = '0';
+
+            setTimeout(() => {
+                form.style.transform = 'translateY(-150%)';
+            }, 200);
+        }
 
         closeForm.addEventListener('click', function() {
-            emailOpen = false;
-            form.style.transform = 'translateY(-150%)';
-            closeForm.style.transform = 'translateX(-6rem)';
-            closeForm.style.opacity = '0';
-            sendEmail.style.transform = 'translateX(6rem)';
-            sendEmail.style.opacity = '0';
+            dismissEmail()
         })
+
+        sendEmail.addEventListener('click', function() {
+            dismissEmail()
+        })
+
+
+        form.style.transition = 'transform 0.6s cubic-bezier(0.4, 0, 0.2, 1)';
     }
 }
 
@@ -152,3 +212,32 @@ function calculate_age(dob) {
 }
 
 document.querySelector('#age').innerHTML = calculate_age(new Date(1996, 5, 29))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
